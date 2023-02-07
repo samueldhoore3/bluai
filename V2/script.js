c09795fee2425a411c2a0029b3665f99a8a9fcc0
samueldhoore3 <119854539+samueldhoore3@users.noreply.github.com>
2023-02-07 01:37:35 +0100

    Add files via upload

---
+++ V2/script.js
@@ -0,0 +1,22 @@
+// sticky nav
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+// slide overlay
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
